d098c2c19740f1c0ae5bff4fd2c106dca57c79ed
6655449987 <6655449987qq@gmail.com>
2015-05-27 21:52:43 +0800

    firstAdd

---
+++ assets/www/js/controllers.js
@@ -0,0 +1,78 @@
+angular.module('starter.controllers', [])
+
+.service('userService', function(){
+	this.initParam = function(){
+		$scope.loginFlag = false;
+		$scope.userName = "";
+		$scope.gender = "";
+	};
+	this.login = function(loginName, password, $scope){
+		console.log('loginName: '+loginName + '; password: '+password);
+		if(loginName == 'user' && password == 'pass'){
+			$scope.userName = "嗡汤圆";
+			$scope.gender = "男";
+			$scope.loginFlag = true;
+			return true;
+		} else {
+			return false;
+		}
+	};
+})
+
+
+.service('messageService', function(){
+	this.alert = function(message){
+		alert(message);
+	}
+})
+
+.controller('AppCtrl', function($scope, $ionicModal, $http, userService , messageService) {
+  
+  // Form data for the login modal
+  $scope.loginData = {};
+  
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+    if(userService.login($scope.loginData.username, $scope.loginData.password, $scope)){
+    	$scope.$apply();
+    	console.log('flag: '+$scope.loginFlag);
+    	$scope.closeLogin();
+    } else {
+    	messageService.alert('用户名或者密码错误');
+    }
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
+
+
